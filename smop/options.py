e5530beae7174c42d3f71f8e76350d17ba4f1068
Xezlec <xezlec@yahoo.com>
2019-03-06 21:35:53 -0600

    Added a minimal options module for now.

---
+++ smop/options.py
@@ -0,0 +1,9 @@
+debug        = '';  # include P to debug parser.
+
+# passed as the debug arg to the PLY parser object.  can be 1 to turn on
+# debugging or can be a log object.
+debug_parser = 0;
+
+no_comments  = False;
+testing_mode = False;
+filename     = 'file';  # only for error reporting.
